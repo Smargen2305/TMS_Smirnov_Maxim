c8fc153911a57126c621dc0e809461e6e8a1ed8e
Smargen2305 <89222229420sma@gmail.com>
2024-12-13 11:14:31 +0500

    Первое и третье задание ДЗ по C#

---
+++ homeWork_Smirnov_Maxim/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace homeWork_Smirnov_Maxim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите Вашу фамилию:");
+            var secondName = Console.ReadLine();
+
+            Console.WriteLine("Введите Ваше имя:");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Введите Ваше отчество:");
+            var middleName = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения в формате (ДД.ММ.ГГГГ)");
+            var birthDay = Console.ReadLine();
+
+            Console.WriteLine($"Ваши данные: {secondName} {firstName} {middleName}, дата рождения: {birthDay}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ homeWork_Smirnov_Maxim_3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace homeWork_Smirnov_Maxim_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, друг! Давай сыграем в игру. Напиши 1, если согласен.");
+            var beginNumber = int.Parse(Console.ReadLine());
+
+            if (beginNumber == 1)
+            {
+                Console.WriteLine("Отличный выбор, я всегда хотел с тобой поиграть!");
+                Console.WriteLine("Итак, представь, что ты находишься в лесу. Перед тобой два пути: налево и направо. Куда бы ты пошел?");
+                Console.WriteLine("Введите ваш выбор: налево или направо");
+
+                var userChoice = Console.ReadLine();
+
+                if (userChoice == "налево")
+                {
+                    Console.WriteLine("Ха-ха-ха, вот ты и попал в ловушку");
+                }
+                if (userChoice == "направо")
+                {
+                    Console.WriteLine("Ты встретил девушку. Вы поженились. Конец твоей игре!!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Мне очень жаль, прощай!");
+            }
+
+
+
+        }
+    }
+}
